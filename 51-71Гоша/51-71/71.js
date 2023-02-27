d08a4ed5b4c71441fd3642cf15cfcfdb9e30d03c
Кирилл Бархатов <117072370+KIrill221Barhatov@users.noreply.github.com>
2023-02-27 17:58:17 +0500

    Add files via upload

---
+++ 51-71Гоша/51-71/71.js
@@ -0,0 +1,15 @@
+// 71.1
+// let obj = { x: 1, y: 2, z: 3 };
+// console.log(obj.x);
+// 71.2
+// let obj = {x: 1, y: 2, z: 3};
+// let key = 'x';
+// console.log(key);
+// 71.3
+// let obj = {x: 1, y: 2, z: 3};
+// let sum = obj.x + obj.y + obj.z;
+// console.log(sum);
+// 71.4
+// let obj = { x: 1, y: 2, z: 3 };
+// let length = Object. keys(obj). length;
+// console.log(length)
